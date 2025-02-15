2f8f471e496cc579693cccb3504217b782ab2579
Jusauria <mewtumew1@gmail.com>
2025-02-15 18:59:10 +0100

    nickname pet

--- cogs/pet-commands.py
+++ cogs/pet-commands.py
@@ -6,7 +6,7 @@ from bot_util import make_embed
 from pojos.emoji_handle import app_emojis, get_emoji, get_item_emoji
 from utilities.profile import show_pet_profile
 from mongodb.owner import get_owner
-
+from mongodb.pet import nicknamePet
 subgroup = "pet_"
 
 class Pet(commands.Cog):
@@ -22,7 +22,18 @@ class Pet(commands.Cog):
             await ctx.response.send_message(embed=make_embed("You are not a Pet Owner"), ephemeral=True)
             return
         await show_pet_profile(ctx,user_data) 
-
+        
+    @discord.slash_command(name=subgroup+"nickname",description="Name your pet :D")
+    async def petNickname(self, ctx: discord.ApplicationContext, nickname:str):
+        user_id = ctx.author.id
+        user_data = get_owner(user_id=user_id)
+        if not user_data:
+            await ctx.response.send_message(embed=make_embed("You are not a Pet Owner"), ephemeral=True)
+            return
+        result = nicknamePet(user_id, nickname) 
+        if result == 0:
+            await ctx.response.send_message(embed=make_embed("Naming Failed"), ephemeral=True)
+        await ctx.response.send_message(embed=make_embed("Your pet is now called "+nickname))
 
 def setup(bot):
     bot.add_cog(Pet(bot))
--- mongodb/pet.py
+++ mongodb/pet.py
@@ -1,11 +1,10 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-from services.pokemon_api import get_growrate, get_nature, get_pokemon, get_gender
 import random
 
 client = MongoClient("mongodb+srv://mewtumew1:hoihoihoi@cluster0.jpxvquh.mongodb.net/test")
 db = client["Pointlink"] 
 owner_coll = db["owner"] 
 
-def nicknamePet(userId,species, name):
-    
+def nicknamePet(userId, name):
+    return owner_coll.update_one({"user_id":userId},{"$set":{"pet.0.nickname": name}})
